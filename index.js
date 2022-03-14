8eb1b4fc0562d8ea6113dc62eb40baeef2891051
Phillupz <phillup.zukowski@gmail.com>
2022-03-14 16:20:07 -0400

    commit tree d37e6b7d047e931bc9e7807243eb6bb298fc106f

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 48;
+const num4 = 12;
 
-// Write your code here
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() +1);
+
+let mod = num3/num4;
+
+let max = Math.max(1, 5, 10, 15, 20);
+
+
+
+
+
